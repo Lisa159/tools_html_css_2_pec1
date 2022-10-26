6feafb246a60c5082b7372fe9cd66651f30d79fa
Aida Maria Gonzalez Fernandez <amgonzalez96@uoc.edu>
2022-10-26 23:29:14 +0200

    First draft of website

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,7 +10,30 @@
  * Write any other JavaScript below
  */
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
+function scrollToSidenav() {
+  document.getElementById("nav-bar").scrollIntoView({ behavior: "smooth" });
+}
+
+function handleNavBar() {
+  const element = document.getElementsByClassName("sidebarjs--is-visible")[0];
+  element.classList.remove("sidebarjs--is-visible");
+}
+
+const slider_viewport = (index, value) => {
+  const observer_slider = new IntersectionObserver(
+    function (entries) {
+      if (entries[0].isIntersecting === true)
+        document.getElementsByClassName("skills__slider-fill")[
+          index
+        ].style.width = `${value}%`;
+    },
+    { threshold: [1] }
+  );
+
+  observer_slider.observe(
+    document.getElementsByClassName("skills__slider")[index]
+  );
+};
+
+slider_viewport(0, 100);
+slider_viewport(1, 85);
